3897e105cd2ac940dcdb0d49cc51c595de0d4847
Abdul Malik kakar <55408067+Abdulmalik02@users.noreply.github.com>
2022-04-11 10:49:48 -0700

    Add files via upload

---
+++ Josephus_problem.py
@@ -0,0 +1,11 @@
+def j(n, k):
+	p, i, seq = list(range(n)), 0, []
+	while p:
+		i = (i+k-1) % len(p)
+		seq.append(p.pop(i))
+	return 'Prisoner killing order: %s.\nSurvivor: %i' % (', '.join(str(i) for i in seq[:-1]), seq[-1])
+ 
+print(j(5, 2))
+
+Survivor: 2
+print(j(41, 3))
